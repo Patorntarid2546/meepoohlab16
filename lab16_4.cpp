04133edc6a961dac2f1f19759f25cfb71b30f2d1
Patorntarid2546 <chpooha@gmail.com>
2022-01-16 01:59:17 +0700

    finish

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &m,int &n,int &o,int &p);
+void shuffle(int *m,int *n,int *o,int *p);
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,14 +11,15 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d);
+	    shuffle(&a,&b,&c,&d);
 	    cout << a << " " << b << " " << c << " " << d << "\n";
-	return 0;
-
 	}
+	
+	return 0;
 }
-	void shuffle(int&m,int &n,int &o,int &p){
-		int qwerty[4]={m,n,o,p};
+
+void shuffle(int*m,int *n,int *o,int *p){
+		int qwerty[4]={*m,*n,*o,*p};
 		int ytrewq[4] = {0,0,0,0};
 	for(int i=0;i<4;i++){
 		ytrewq[i] = rand()%4;
@@ -29,9 +30,8 @@ int main(){
 			}
 		}
 	}
-	m = qwerty[ytrewq[0]];
-	n = qwerty[ytrewq[1]];
-	o = qwerty[ytrewq[2]];
-	p = qwerty[ytrewq[3]];
+	*m = qwerty[ytrewq[0]];
+	*n = qwerty[ytrewq[1]];
+	*o = qwerty[ytrewq[2]];
+	*p = qwerty[ytrewq[3]];
 }
-	
